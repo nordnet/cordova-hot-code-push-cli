6eed13137d972b1f382829a2fede232696c30777
Andreas Hassellof <andreas@ombori.com>
2015-07-22 11:06:41 +0200

    initial commit

---
+++ chcp.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+var path = require('path'),
+    argv = require('optimist').argv;
+
+var cmd = argv._[0];
+switch(cmd) {
+  case 'init':
+    console.log('Running '+cmd);
+    var command = require(path.join(__dirname,'lib',cmd+'.js'));
+    break;
+  default:
+    console.log('Should print usage instructions.');
+    process.exit(0);
+}
+
+command.execute(argv);
