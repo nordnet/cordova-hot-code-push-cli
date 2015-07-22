4c54fdd54438da9c9a768f73d9370ece91c88959
Andreas Hassellof <andreas@ombori.com>
2015-07-22 12:04:41 +0200

    support for chcp login to create credentials file

--- chcp.js
+++ chcp.js
@@ -4,6 +4,7 @@ var path = require('path'),
 
 var cmd = argv._[0];
 switch(cmd) {
+  case 'login':
   case 'init':
     console.log('Running '+cmd);
     var command = require(path.join(__dirname,'lib',cmd+'.js'));
