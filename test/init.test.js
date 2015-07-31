0e3bb2d2fc4f651175547ca22574dd96a5557a12
Dmitry Demyankov <dmitry.demyankov@gmail.com>
2015-08-01 00:30:50 +0200

    Basic unit test for init module

--- test/init.test.js
+++ test/init.test.js
@@ -1,24 +1,58 @@
-import { execute } from './../lib/init';
 import prompt from 'prompt';
 
 import sinon from 'sinon';
 import { expect } from 'chai';
 
+import { execute } from './../src/init';
+
+const withBucket = {
+  name: 'name',
+  s3region: 'us-east-1',
+  s3bucket: 'bucket',
+  ios_identifier: 'ios',
+  android_identifier: 'android',
+  update: 'resume',
+};
+
+const contentUrl = {
+  content_url: 'http://url',
+};
+
+const expectedContentWithBucket = {
+  name: 'name',
+  s3region: 'us-east-1',
+  s3bucket: 'bucket',
+  ios_identifier: 'ios',
+  android_identifier: 'android',
+  update: 'resume',
+  content_url: 'https://s3.amazonaws.com/bucket',
+};
+
 describe('init', () => {
   let sandbox;
-  let spy;
+  let get;
 
-  beforeEach(() => {
-    sandbox = sinon.sandbox.create();
-    sandbox.stub(prompt, 'start');
-    spy = sandbox.spy(prompt, 'get');
-    execute();
-  });
-
+  beforeEach(() => sandbox = sinon.sandbox.create());
   afterEach(() => sandbox.restore());
 
-  it('should init', () => {
-    console.log(spy.args[0][1]);
+  describe('when bucket is set', () => {
+    beforeEach(() => {
+      get = sandbox.stub(prompt, 'get', stubPromptGet(withBucket));
+      execute();
+    });
+
+    it('should call prompt.get once', () => expect(get.calledOnce).to.be.true);
+
+    // TODO stub fs.writeFile and verify contents
   });
-  it('should fail', () => expect('1').to.equal('2'));
 });
+
+function stubPromptGet(result) {
+  return function(props, callback) {
+    if (props.properties && props.properties.s3region) {
+      callback(null, result);
+    } else if (props.properties && props.properties.content_url) {
+      callback(null, contentUrl);
+    }
+  }
+}
