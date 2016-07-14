13a8c49b51a52018346a7444038719a4adc727df
Nikolay Demyankov <nikolay.demyankov@nordnet.se>
2016-07-14 12:48:06 +0200

    Refactored utils script: es6 syntax + added more methods.

--- dist/utils.js
+++ dist/utils.js
@@ -3,33 +3,70 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.getInput = getInput;
-exports.writeFile = writeFile;
 
-var _fs = require('fs');
+var _pify = require('pify');
 
-var _fs2 = _interopRequireDefault(_fs);
+var _pify2 = _interopRequireDefault(_pify);
+
+var _fs2 = require('fs');
+
+var _fs3 = _interopRequireDefault(_fs2);
+
+var _recursiveReaddir = require('recursive-readdir');
+
+var _recursiveReaddir2 = _interopRequireDefault(_recursiveReaddir);
+
+var _hidefile = require('hidefile');
+
+var _hidefile2 = _interopRequireDefault(_hidefile);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function getInput(prompt, props) {
+var fs = (0, _pify2.default)(_fs3.default);
+
+var getInput = function getInput(prompt, props) {
   return new Promise(function (resolve) {
     return prompt.get(props, function (err, result) {
       return resolve(result, err);
     });
   });
-}
+};
 
-function writeFile(file, content) {
+var readFile = function readFile(file) {
+  return fs.readFile(file, 'utf8').then(function (content) {
+    return JSON.parse(content);
+  });
+};
+
+var writeFile = function writeFile(file, content) {
+  var str = JSON.stringify(content, null, 2);
+
+  return fs.writeFile(file, str, 'utf8');
+};
+
+var nonHiddenFile = function nonHiddenFile(file) {
+  return !_hidefile2.default.isHiddenSync(file);
+};
+
+var readDir = function readDir(dir, ignoredFiles) {
   return new Promise(function (resolve, reject) {
-    var data = JSON.stringify(content, null, 2);
-    _fs2.default.writeFile(file, data, function (err) {
+    (0, _recursiveReaddir2.default)(dir, ignoredFiles, function (err, files) {
       if (err) {
         reject(err);
       } else {
-        resolve();
+        resolve(files);
       }
     });
+  }).then(function (files) {
+    return files.filter(nonHiddenFile);
   });
-}
+};
+
+exports.default = {
+  getInput: getInput,
+  readFile: readFile,
+  writeFile: writeFile,
+  readDir: readDir
+};
+module.exports = exports['default'];
 //# sourceMappingURL=utils.js.map
--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,44 @@
-import fs from 'fs';
+import pify from 'pify';
+import _fs from 'fs';
+import recursive from 'recursive-readdir';
+import hidefile from 'hidefile';
 
-export function getInput(prompt, props) {
+const fs = pify(_fs);
+
+const getInput = (prompt, props) => {
   return new Promise(resolve => prompt.get(props, (err, result) => resolve(result, err)));
+};
+
+const readFile = (file) => {
+  return fs.readFile(file, 'utf8').then(content => JSON.parse(content));
+};
+
+const writeFile = (file, content) => {
+  const str = JSON.stringify(content, null, 2);
+
+  return fs.writeFile(file, str, 'utf8');
 }
 
-export function writeFile(file, content) {
-  return new Promise((resolve, reject) => {
-    var data = JSON.stringify(content, null, 2);
-    fs.writeFile(file, data, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
+const nonHiddenFile = (file) => {
+  return !hidefile.isHiddenSync(file);
 }
+
+const readDir = (dir, ignoredFiles) => {
+  return new Promise((resolve, reject) => {
+      recursive(dir, ignoredFiles, (err, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(files);
+        }
+      });
+    })
+    .then(files => files.filter(nonHiddenFile));
+};
+
+export default {
+  getInput,
+  readFile,
+  writeFile,
+  readDir
+};
